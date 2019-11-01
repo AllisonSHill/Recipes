54e5a0951bdc05ea9a4785546feb24ff4f4d3e68
allisonhill00 <allison.m.s.hill@gmail.com>
2019-11-01 09:26:14 -0600

    Add sorting to ingredients

--- Pages/Ingredients/Index.cshtml.cs
+++ Pages/Ingredients/Index.cshtml.cs
@@ -23,16 +23,27 @@ namespace RazorPagesRecipe.Pages.Ingredients
         public IList<Ingredient> Ingredient { get;set; }
         [BindProperty(SupportsGet = true)]
         public string SearchString { get; set; }
-        public List<SelectListItem> recipeList { get; set; }
+        public SelectList RecipeNames { get; set; }
+        [BindProperty(SupportsGet = true)]
+        public string SelectedRecipe { get; set; }
 
         public async Task OnGetAsync()
         {
+            IQueryable<string> recipeQuery = from i in _context.Ingredient
+                                           orderby i.recipeName
+                                           select i.recipeName;
+
             var ingredients = from i in _context.Ingredient
                           select i;
             if (!string.IsNullOrEmpty(SearchString))
             {
                 ingredients = ingredients.Where(s => s.Name.Contains(SearchString));
             }
+            if (!string.IsNullOrEmpty(SelectedRecipe))
+            {
+                ingredients = ingredients.Where(x => x.recipeName == SelectedRecipe);
+            }
+            RecipeNames = new SelectList(await recipeQuery.Distinct().ToListAsync());
             Ingredient = await ingredients.ToListAsync();
         }
     }
